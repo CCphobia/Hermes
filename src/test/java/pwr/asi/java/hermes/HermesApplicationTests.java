da6d8f5a111700ecde2b19439c3ea3df1cd2f901
CCphobia <peteros9999@gmail.com>
2019-11-18 05:24:53 +0100

    Root Spring Init Project

---
+++ src/test/java/pwr/asi/java/hermes/HermesApplicationTests.java
@@ -0,0 +1,13 @@
+package pwr.asi.java.hermes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
